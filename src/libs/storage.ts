c22f37763862b532bda3376b983fd1205f70d1db
Felipe Jung <felipemattoseu@gmail.com>
2021-04-23 11:01:32 -0400

    Add notification to warn about watering plant

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Notifications from "expo-notifications";
 import { format } from "date-fns";
 
 import { Plant } from "../types";
@@ -6,17 +7,51 @@ import { Plant } from "../types";
 export type StoragePlantProps = {
   [id: string]: {
     data: Plant;
+    notificationId: string;
   };
 };
 
 export async function savePlant(plant: Plant): Promise<void> {
   try {
+    const nextTime = new Date(plant.dateTimeNotification);
+    const now = new Date();
+
+    const { times, repeat_every } = plant.frequency;
+
+    if (repeat_every === "week") {
+      const interval = Math.trunc(7 / times);
+      nextTime.setDate(now.getDate() + interval);
+    } else {
+      nextTime.setDate(nextTime.getDate() + 1);
+    }
+
+    const seconds = Math.abs(
+      Math.ceil((now.getTime() - nextTime.getTime()) / 1000)
+    );
+
+    const notificationId = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Heey 🌱",
+        body: `Está na hora de cuidar da sua ${plant.name}`,
+        sound: true,
+        priority: Notifications.AndroidNotificationPriority.HIGH,
+        data: {
+          plant,
+        },
+      },
+      trigger: {
+        seconds: seconds < 60 ? 60 : seconds,
+        repeats: true,
+      },
+    });
+
     const data = await AsyncStorage.getItem("@plantmanager:plants");
     const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
     const newPlant = {
       [plant.id]: {
         data: plant,
+        notificationId,
       },
     };
 
@@ -58,6 +93,10 @@ export async function loadPlants(): Promise<Plant[]> {
 export async function removePlant(id: string): Promise<void> {
   const data = await AsyncStorage.getItem("@plantmanager:plants");
   const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+  await Notifications.cancelScheduledNotificationAsync(
+    plants[id].notificationId
+  );
 
   delete plants[id];
 
